e8bd4a6df01595175592752a2f3e874ecf678f8f
Abaynew Guadie Nigussie <113917030+abaynew-guadie@users.noreply.github.com>
2022-10-19 11:21:58 +0200

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,24 +2,14 @@
  * File: 1-alphabet.c
  * 
 Auth: Abaynew g. */
-
-
-#include "main.h"  /** "holberton.h"
-
-
- * print_alphabet - prints the alphabet in lowercase, followed by a new line.
+#include "main.h" 
+/*
+* print_alphabet - prints the alphabet in lowercase, followed by a new line.
  */
-
 void print_alphabet(void)
 {
-
  char letter;
-
-	
  for (letter = 'a'; letter <= 'z'; letter++)
-	
  	_putchar(letter);
-
-	
        _putchar('\n');
 }
