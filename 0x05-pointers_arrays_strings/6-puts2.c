7d85585daaf0f15043aa8b2ef7a1c79c7f80603d
abaynew-guadie <abaynewhu@yahoo.com>
2022-10-24 13:49:36 -0700

    task 6 files

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
- * puts2 - Prints one char out of two of a strin
- *taining characters./
+* puts2 - Prints one char out of two of a strin
+*taining characters./
 */
 void puts2(char *str)
 {
 int i, len = 0; 
- while (str[i++])
+while (str[i++])
 len++;
-for (i = 0; i < len; i +=2)   
+for (i = 0; i < len; i+=2)   
 _putchar(str[i]);
 _putchar('\n');
 }
