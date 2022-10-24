906ceb71e80853d35ec5db295aa289fc709c3b69
abaynew-guadie <abaynewhu@yahoo.com>
2022-10-24 06:51:03 -0700

    task 0 98 Battery st

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,9 @@
 /*
- * Project : 0. 98 Battery st.
- */
-
+* Project : 0. 98 Battery st.
+*/
 #include "main.h"
 /**
- * reset_to_98 - resets value of pointer to 98
+* reset_to_98 - resets value of pointer to 98
 * @n: pointer to reset to 98
 * Return: void
 */
