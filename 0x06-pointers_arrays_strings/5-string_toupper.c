aef6efd9fc93233ce724015bbd9ae9f696b2efc3
abaynew-guadie <abaynewhu@yahoo.com>
2022-10-27 02:37:13 -0700

    task 5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -6,13 +6,13 @@
 */
 char *string_toupper(char *a)
 {
-	int i;
+int i;
 for (i = 0; a[i] != 0; i++)
-	{
+{
 if (a[i] > 90 && a[i] != 10)
-	{
-		a[i] = a[i] - 32;
-	}
-	}
-	return (a);
+{
+a[i] = a[i] - 32;
+}
+}
+return (a);
 }
