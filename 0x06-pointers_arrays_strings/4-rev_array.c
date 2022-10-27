a1df509f0e2cb2afef64b54f82d60a4a19cf5d51
abaynew-guadie <abaynewhu@yahoo.com>
2022-10-27 02:26:45 -0700

    task 4 files

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -8,15 +8,15 @@
 */
 void reverse_array(int *a, int n)
 {
-	int i, j, d;
-	if (n % 2 != 0)
-		d = n + 1;
-	else
-		d = n;
-	for (i = 0; i < d / 2; i++)
-	{
-		j = a[i];
-		a[i] = a[n - 1 - i];
-		a[n - 1 - i] = j;
-	}
+int i, j, d;
+if (n % 2 != 0)
+d = n + 1;
+else
+d = n;
+for (i = 0; i < d / 2; i++)
+{
+j = a[i];
+a[i] = a[n - 1 - i];
+a[n - 1 - i] = j;
+}
 }
