2c08d336c9a7dbafbcc8e91e819efd62a4dfc2c2
abaynew-guadie <abaynewhu@yahoo.com>
2022-10-27 14:20:26 -0700

    task 2 strncpy files

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,15 +9,18 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 int i = 0;
+
+
+
 while (i < n && src[i] != '\0')
-	{
- dest[i] = src[i];
-	i++;
-	}
+{
+dest[i] = src[i];
+i++;
+}
 if (i < n)
- {
- for (; i < n; i++)
-	dest[i] = '\0';
-	}
+{
+for (; i < n; i++)
+dest[i] = '\0';
+}
 return (dest);
 }
