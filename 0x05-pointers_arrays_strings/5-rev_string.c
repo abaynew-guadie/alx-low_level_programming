3bce007d00ecfa5cf9c5ed5c7056dce7a9d8cd07
abaynew-guadie <abaynewhu@yahoo.com>
2022-10-29 03:13:05 -0700

    task 6

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
 * 5-rev_string - Reverses a string.
-* rev_string is a function
+*@s rev_string is a function
 * i to be reserved.0
-* rev_string  the char on parameter of poniter s
+* @S the  char on parameter of poniter s
 */
 void rev_string(char *s)
 {
