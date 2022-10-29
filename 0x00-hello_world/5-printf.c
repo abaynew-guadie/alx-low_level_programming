8247d13988d317d6160aa150515c7001eaad722e
abaynew-guadie <abaynewhu@yahoo.com>
2022-10-29 15:14:37 -0700

    task 5:Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,15 +1,14 @@
-/*
- * File: 5-printf.c
+/**
+* File: 5-printf.c
 * Auth: Abaynew G.
 */
 #include <stdio.h>
 /**
-* main - Prints "with proper grammar, but the outcome is
-* a piece of art," followed by a new line.
-* Return: Always 0.
+ *main - Entry point
+* Return: Always 0 (Success)
 */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");	
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
