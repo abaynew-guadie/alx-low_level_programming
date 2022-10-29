baa3463c4f4a71076d1d7c10cc664e2428a687db
abaynew-guadie <abaynewhu@yahoo.com>
2022-10-29 15:01:56 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,15 @@
 /*
- * File: 4-puts.c
- * Auth: abaynew G. */
+* File: 4-puts.c
+* Auth: abaynew G.
+*/
 #include <stdio.h>
 /**
- * main - Prints "Programming is like building a multilingual
- * puzzle, followed by a new line.
-  * Return: Always 0.
- */
+* main - Prints "Programming is like building a multilingual
+* puzzle, followed by a new line.
+* Return: Always 0.
+*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+return (0);
 }
