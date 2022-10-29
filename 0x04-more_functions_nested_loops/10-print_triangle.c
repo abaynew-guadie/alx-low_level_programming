c05b74ccf417d1d3118ef3305458c5847da67932
abaynew-guadie <abaynewhu@yahoo.com>
2022-10-29 07:11:16 -0700

    task 10:Triangles

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -6,18 +6,18 @@
 */
 void print_triangle(int size)
 {
- int i, j;
-   if (size <= 0)
-		_putchar('\n');
-	for (i = 1; i <= size; i++)
-	{
-	for (j = 1; j <= size; j++)
-	{
-	if (j <= (size - i))
-		_putchar(' ');
-	else
-	_putchar('#');
-	}
-    _putchar('\n');
-	}
+int i, j;
+if (size <= 0)
+_putchar('\n');
+for (i = 1; i <= size; i++)
+{
+for (j = 1; j <= size; j++)
+{
+if (j <= (size - i))
+_putchar(' ');
+else
+_putchar('#');
+}
+_putchar('\n');
+}
 }
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -1,1 +1,1 @@
-program that prints the numbers from 1 to 100, followed by a new line in fizz buzz 
+ function that prints a triangle, followed by a new line.
