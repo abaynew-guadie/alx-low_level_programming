30ec4ef724d1338ca8bd0d21560ba5ba6e60ebfb
abaynew-guadie <abaynewhu@yahoo.com>
2022-10-29 03:01:14 -0700

    task 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,14 +1,15 @@
 #include "main.h"
 /**
 * 5-rev_string - Reverses a string.
-* string to be reserved.0
-* rev_string  to displays
+* rev_string is a function
+* i to be reserved.0
+* rev_string  the char on parameter of poniter s
 */
 void rev_string(char *s)
 {
-int i = 0, len = 0; 
+int i = 0, len = 0;
 char tmp;
-while (s[i++])  
+while (s[i++])
 len++;
 for (i = len - 1; i >= len / 2; i--)
 {
