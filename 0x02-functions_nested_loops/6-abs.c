0c12bcaa692b823472cfcdf42f1e8730c111daeb
abaynew-guadie <abaynewhu@yahoo.com>
2022-12-12 22:54:47 -0800

    task 6:There is no such thing as absolute value

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,11 +1,8 @@
-/**
-* File: 6-abs.c
-* Auth: Abaynew G.
-*/
 #include "main.h"
 /**
-* _abs - Computes the absolute value of an integer.
+*_abs - Computes the absolute value of an integer.
 * @n: The integer to be computed.
+*
 * Return: The absolute value of the integer.
 */
 int _abs(int n)
