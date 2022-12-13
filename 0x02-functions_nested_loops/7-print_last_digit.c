4db25595729efbbaba85da3588ffa5481c5ddb1e
abaynew-guadie <abaynewhu@yahoo.com>
2022-12-12 23:00:30 -0800

    task 7:There are only 3 colors, 10 digits, and 7 notes

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,11 +1,8 @@
-/**
-* File: 7-print_last_digit.c
-* Auth: Abaynew G.
-*/
 #include "main.h"
 /**
-* print_last_digit - Prints the last digit of a number.
-* @n: The number in question.
+*print_last_digit - Prints the last digit of a number.
+*@n: The number in question.
+*
 * Return: Value of the last digit.
 */
 int print_last_digit(int n)
