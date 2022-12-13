cefc9f3ea66226639149ca346e8c8be61dadf7e4
abaynew-guadie <abaynewhu@yahoo.com>
2022-12-12 22:09:12 -0800

    task 1:I sometimes suffer from insomnia

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,7 @@
-/**
-* File: 1-alphabet.c
-* Auth: Abaynew g.
-*/
 #include "main.h"
 /**
-* print_alphabet: prints the alphabet in lowercase, followed by a new line.
+* print_alphabet - Print lowercase alphabet
+* followed by a new line.
 */
 void print_alphabet(void)
 {
