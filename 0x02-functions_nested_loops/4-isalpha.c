bda431094d17f71f3b110b1fce0f385b302c2ab1
abaynew-guadie <abaynewhu@yahoo.com>
2022-12-12 22:42:33 -0800

    task 4:isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,11 +1,8 @@
-/**
-* File: 4-isalpha.c
-* Auth: Abaynew G.
-*/
 #include "main.h"
 /**
-* _isalpha - Checks if a character is alphabetic.
-*  @c: The character to be checked.
+*_isalpha - Checks a character is alphabetic
+*@c: The character to be checked
+*
 * Return: 1 if character is letter, lowercase or uppercase, 0 otherwise.
 */
 int _isalpha(int c)
