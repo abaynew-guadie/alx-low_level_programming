f814ef109e4a73295d0f6378fd3ed5ab65a42e9b
abaynew-guadie <abaynewhu@yahoo.com>
2022-12-14 00:47:25 -0800

    task 1:Like, comment, subscribe

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,15 +1,15 @@
 #include "main.h"
 #include <stdio.h>
 /**
-* main - Print an infinite loop
+* main - Print cause of an infinite loop
 *
 * Return: 0
 */
 int main(void)
 {
 int i;
-printf ("Infinite loop incoming :(\n");
+printf("Infinite loop incoming :(\n");
 i = 0;
-printf ("Infinite loop avoided! \\o/\n");
+printf("Infinite loop avoided! \\o/\n");
 return (0);
 }
