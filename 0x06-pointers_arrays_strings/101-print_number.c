770e24a8759dc80d1954e39f55fabf7b0141a779
abaynew-guadie <abaynewhu@yahoo.com>
2022-12-21 13:08:24 -0800

    task 9:Numbers have life

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_number - Prints an integrer
+ * @n: The integer to  print
+ */
+void print_number(int n)
+{
+unsigned int num = n;
+if (n < 0)
+{
+_putchar('-');
+num = -num;
+}
+if ((num / 10) > 0)
+print_number(num / 10);
+_putchar((num % 10) + '0');
+}
