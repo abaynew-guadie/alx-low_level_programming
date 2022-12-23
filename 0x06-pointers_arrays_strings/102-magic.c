5cca059446c8cfaa10f5ebbcc9c760d774b52515
abaynew-guadie <abaynewhu@yahoo.com>
2022-12-22 21:55:21 -0800

    task 10:magic files

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,8 +1,4 @@
 #include <stdio.h>
-/**
- * main - Print the function of void
- * Return: Always 0.
-*/
 int main(void)
 {
 int n;
@@ -11,8 +7,7 @@ int *p;
 a[2] = 1024;
 p = &n;
 /*
-* write your line of code here...
-*  Remember:
+* write your line of code here... *  Remember:
 *  - you are not allowed to use a
 *- you are not allowed to modify p
 * - only one statement
