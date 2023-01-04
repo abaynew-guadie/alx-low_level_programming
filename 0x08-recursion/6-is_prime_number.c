7f4da131a0d63bddffc24e976073d88524605ce8
abaynew-guadie <abaynewhu@yahoo.com>
2023-01-04 05:03:54 -0800

    task 6:Inception. Is it possible?

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,7 +1,3 @@
-/*
-*  File: 6-is_prime_number.c
-* Auth: Abaynew G.
-*/
 #include "main.h"
 int is_divisible(int num, int div);
 int is_prime_number(int n);
@@ -12,7 +8,6 @@ int is_prime_number(int n);
  *
  * Return: If the number is divisible - 0.
  * If the number is not divisible - 1.
- *
  */
 int is_divisible(int num, int div)
 {
@@ -22,8 +17,9 @@ if (div == num / 2)
 return (1);
 return (is_divisible(num, div + 1));
 /**
- * is_prime_number - Checks if a number is prime.
+ * is_prime_number - Prints the prime numbers
  * @n: The number to be checked.
+ *
  * Return: If the integer is not prime - 0.
  * If the number is prime - 1.
  */
@@ -35,4 +31,5 @@ return (0);
 if (n >= 2 && n <= 3)
 return (1);
 return (is_divisible(n, div));
+}
 }
