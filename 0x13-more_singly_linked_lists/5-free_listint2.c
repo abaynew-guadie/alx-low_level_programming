b3d04b01abbc7d20a5178f1d37d6d3340f69582f
abaynew-guadie <abaynewhu@yahoo.com>
2023-01-31 04:39:42 -0800

    task 5: Free

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,6 +1,5 @@
 #include <stdlib.h>
 #include "lists.h"
-
 /**
  * free_listint2 - ...
  * @head: ...
@@ -9,21 +8,20 @@
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *temp;
-
-	if (head)
-	{
-		while (*head)
-		{
-			temp = (*head);
-			*head = (*head)->next;
-			ree(temp);
-		}
-	}
-	else
-	{
-		return;
-	}
-	free(*head);
-	head = 0;
+listint_t *temp;
+if (head)
+{
+while (*head)
+{
+temp = (*head);
+*head = (*head)->next;
+free(temp);
+}
+}
+else
+{
+return;
+}
+free(*head);
+head = 0;
 }
