469ec9003756f39cfa762cf3c4a220fd0e3b4870
abaynew-guadie <abaynewhu@yahoo.com>
2023-01-31 05:43:42 -0800

    task 12:Print (safe version)

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include "lists.h"
 /**
@@ -7,52 +8,29 @@
  * Return: number of nodes
  */
 size_t print_listint_safe(const listint_t *head)
-
 {
+const listint_t *new, *comp;
+size_t count_new = 0, count_comp = 0;
 
-	const listint_t *new, *comp;
-
-	size_t count_new = 0, count_comp = 0;
-
-	new = head;
-
-	comp = head;
-
-	while (new != NULL)
-
-	{
-
-		while (count_new > count_comp)
-
-		{
-
-			if (new == comp)
-
-			{
-
-				printf("-> [%p] %d\n", (void *)new, new->n);
-
-				return (count_new);
-
-			}
-
-			count_comp++;
-
-			comp = comp->next;
-
-		}
-
-		comp = head;
-
-		count_comp = 0;
-
-		printf("[%p] %d\n", (void *)new, new->n);
-
-		count_new++;
-
-		new = new->next;
-
-	}
-
-	return (count_new);
+new = head;
+comp = head;
+while (new != NULL)
+{
+while (count_new > count_comp)
+{
+if (new == comp)
+{
+printf("-> [%p] %d\n", (void *)new, new->n);
+return (count_new);
+}
+count_comp++;
+comp = comp->next;
+}
+comp = head;
+count_comp = 0;
+printf("[%p] %d\n", (void *)new, new->n);
+count_new++;
+new = new->next;
+}
+return (count_new);
 }
