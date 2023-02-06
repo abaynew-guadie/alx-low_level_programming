465f1fefa80065cb12625a992193d6d9a50d560f
abaynew-guadie <abaynewhu@yahoo.com>
2023-02-06 08:02:09 -0800

    task 4

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -96,11 +96,9 @@ printf("%-35s", "Version:");
 switch (ehdr->e_ident[EI_VERSION])
 {
 case EV_NONE:
-
 printf("%d (invalid)\n", EV_NONE);
 break;
 case EV_CURRENT:
-
 printf("%d (current)\n", EV_CURRENT);
 break;
 }
@@ -117,63 +115,51 @@ switch (ehdr->e_ident[EI_OSABI])
 {
 case ELFOSABI_SYSV:
 printf("UNIX - System V\n");
-
 break;
 case ELFOSABI_HPUX:
 printf("UNIX - HP-UX\n");
 break;
-
 case ELFOSABI_NETBSD:
 printf("UNIX - NetBSD\n");
 break;
 case ELFOSABI_LINUX:
-
 printf("UNIX - Linux\n");
 break;
 case ELFOSABI_SOLARIS:
 printf("UNIX - Solaris\n");
 break;
-
 case ELFOSABI_IRIX:
 printf("UNIX - IRIX\n");
 break;
 case ELFOSABI_FREEBSD:
 printf("UNIX - FreeBSD\n");
-
 break;
 case ELFOSABI_TRU64:
 printf("UNIX - TRU64\n");
 break;
-
 case ELFOSABI_ARM:
 printf("ARM\n");
 break;
 case ELFOSABI_STANDALONE:
-
 printf("Stand-alone (embedded)\n");
 break;
 default:
-
 printf("<unknown: %d>\n", ehdr->e_ident[EI_OSABI]);
 }
 }
-
 /**
  * get_vabi - get ABI version
  * @ehdr: pointer to struct of ELF header features
  */
-
 void get_vabi(Elf64_Ehdr *ehdr)
 {
 printf("%-35s%d\n", "ABI Version:",                                                                                                     ehdr->e_ident[EI_ABIVERSION]);
-
 }
 /**
  * get_type - determine file type
  * @ehdr: pointer to struct of ELF header features
  *
  */
-
 void get_type(Elf64_Ehdr *ehdr)
 {
 printf("%-35s", "Type:");
@@ -181,36 +167,29 @@ switch (ehdr->e_type)
 {
 case ET_NONE:
 printf("NONE (Unknown type)\n");
-
 break;
 case ET_REL:
 printf("REL (Relocatable file)\n");
 break;
 case ET_EXEC:
-
 printf("EXEC (Executable file)\n");
 break;
 case ET_DYN:
 printf("DYN (Shared object file)\n");
-
 break;
 case ET_CORE:
 printf("CORE (Core file)\n");
 break;
 }
 }
-
 /**
  * get_entry - determine entry point function address
  * @ehdr: pointer to struct of ELF header features
  */
-
 void get_entry(Elf64_Ehdr *ehdr)
 {
-
-printf("%-35s0x%lx\n", "Entry point address:",                                                                                          (unsigned long) ehdr->e_entry);
+printf("%-35s0x%lx\n", "Entry point address:", (unsigned long) ehdr->e_entry);
 }
-
 /**
  * main - display information contained in ELF file header
  * @argc: number of arguments
@@ -220,18 +199,14 @@ printf("%-35s0x%lx\n", "Entry point address:",                                  
  */
 int main(int argc, char *argv[])
 {
-
 int fd;
 ssize_t r;
 Elf64_Ehdr *helf;
-
 if (argc != 2)
 {
 dprintf(STDERR_FILENO, "Usage: %s <elf_file>\n", argv[0]);
-
 exit(98);
 }
-
 fd = open(argv[1], O_RDONLY);
 if (fd == -1)
 {
@@ -242,33 +217,36 @@ exit(98);
 helf = malloc(sizeof(Elf64_Ehdr));
 if (helf == NULL)
 {
-dprintf(STDERR_FILENO, "Error: out of memory\n");
-close(fd);
 
+dprintf(STDERR_FILENO, "Error: out of memory\n");
+
+close(fd);
 exit(98);
 }
+
 r = read(fd, helf, sizeof(Elf64_Ehdr));
 if (r == -1)
 {
 free(helf);
+
 dprintf(STDERR_FILENO, "Error: could not read %s\n", argv[1]);
 close(fd);
 exit(98);
 }
-
 if (close(fd))
 dprintf(STDERR_FILENO, "Problem closing fd %d", fd);
 
 get_magic(helf, argv[1], fd);
 get_class(helf);
+
 get_data(helf);
 get_velf(helf);
 
 get_osabi(helf);
 get_vabi(helf);
-get_type(helf);
 
+get_type(helf);
 get_entry(helf);
 free(helf);
-
-exit(EXIT_SUCCESS);}
+exit(EXIT_SUCCESS);
+}
