76fc78b423f067dc1e06cb9e2a155afbd7e3e253
abaynew-guadie <abaynew@yahoo.com>
2023-03-22 02:57:34 -0700

    tasks

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,13 +1,16 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
+#include <stdio.h>
+#include <stddef.h>
+
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
 int _abs(int n);
 int _isupper(int c);
 int _isdigit(int c);
-int _strlen(char *s);
+size_t _strlen(const char *str);
 void _puts(char *s);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
