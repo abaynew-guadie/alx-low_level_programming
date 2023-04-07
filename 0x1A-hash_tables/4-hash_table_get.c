5de961dae72346294fe2a5724267726ac086ef34
abaynew-guadie <abaynew@yahoo.com>
2023-04-07 02:24:45 -0700

    task 4:ht['betty']

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -13,20 +13,18 @@
   */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	unsigned long int idx = 0;
-	hash_node_t *elem = NULL;
+unsigned long int idx = 0;
+hash_node_t *elem = NULL;
 
-	if (ht == NULL || key == NULL)
-		return (NULL);
+if (ht == NULL || key == NULL)
+return (NULL);
 
-	idx = key_index((unsigned char *) key, ht->size);
-	elem = ht->array[idx];
+idx = key_index((unsigned char *) key, ht->size);
+elem = ht->array[idx];
+if (elem == NULL)
+return (NULL);
 
-	if (elem == NULL)
-		return (NULL);
-
-	while (strcmp(key, elem->key) != 0)
-		elem = elem->next;
-
-	return (elem->value);
+while (strcmp(key, elem->key) != 0)
+elem = elem->next;
+return (elem->value);
 }
